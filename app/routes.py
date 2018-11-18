562e19c6f2b9a6179649a327053763e77adc6af8
Dave Randall <dave@goldsquare.co.uk>
2018-11-18 19:19:45 +0000

    End of chapter 2

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,20 @@
 from app import app
-import time
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return("Access a path to get a response after 60 seconds")
+    user = {'username': 'Harry'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/<path:dummy>')
-def other(dummy):
-    time.sleep(60)
-    return("You attempted to access {}".format(dummy))
+
